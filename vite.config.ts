3a3c638c51eaa7c33d8d65a5ce93604ca5ad4593
Raden <hyou3504@gmail.com>
2026-07-27 22:28:31 +0700

    Add Vite configuration for React with proxy settings

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://110.92.72.98:5002',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
